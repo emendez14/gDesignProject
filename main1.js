ac4313c8da241b799496868863a73237718610de
endesnson <endesnsonjr@gmail.com>
2019-04-24 22:00:50 -0400

    played with jscript a bit

--- main1.js
+++ main1.js
@@ -4,6 +4,33 @@ function openMenu () {
   document.getElementById("dropdown").classList.toggle("active");
 }
 
-document.querySelector('.about').onmouseover = function(){
-  section.style.animation = 'mission';
+var about = document.querySelector(".about");
+var projects = document.querySelector(".projects");
+var contact = document.querySelector(".contact");
+var aboutAni1 = document.getElementsByTagName("h1");
+var aboutAni2 = document.getElementsByTagName("p");
+
+function aboutChange (){
+  about.onclick = function(){
+    aboutAni1[1].style.color = "blue";
+    aboutAni2[1].style.color = "blue";
+  }
 };
+
+aboutChange();
+
+function projectsChange (){
+  projects.onclick = function(){
+    aboutAni1[2].style.color = "blue";
+  }
+};
+
+projectsChange();
+
+function contactChange (){
+  contact.onclick = function(){
+    aboutAni1[3].style.color = "blue";
+  }
+};
+
+contactChange();
