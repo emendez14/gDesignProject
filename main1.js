7cce0079f0e1b4f6056a33c08e6f03be008e541a
endesnson <endesnsonjr@gmail.com>
2019-04-24 21:19:34 -0400

    Cleaned some ugly structure, changed font

--- main1.js
+++ main1.js
@@ -3,3 +3,7 @@ document.getElementById("menu").addEventListener("click", openMenu);
 function openMenu () {
   document.getElementById("dropdown").classList.toggle("active");
 }
+
+document.querySelector('.about').onmouseover = function(){
+  section.style.animation = 'mission';
+};
